9ca203f8e65785fff653f0c4e0dd1b7f279c040d
Amr-helmy0 <107793670+Amr-helmy0@users.noreply.github.com>
2023-03-28 16:29:00 +0200

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,152 @@
+const logotext = "Amr helmy";
+const meta = {
+    title: "Amr helmy",
+    description: "I’m Amr helmy  _ Frontend devloper,currently working in Egypet",
+};
+
+const introdata = {
+    title: "I’m Amr helmy",
+    animated: {
+        first: "I Create UI-UX",
+        second: "I develop websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am a talented, ambitious and hardworking person, with extensive web development skills and experience. Furthermore, I am adept at handling",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a talented, ambitious and hardworking person, with extensive web development skills and experience. Furthermore, I am adept at handling.",
+};
+const worktimeline = [{
+        jobtitle: "IT Gates",
+        where: "Egypet",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "UI-UX Figma",
+        value: 90,
+    },
+    {
+        name: "Flutter & Dart",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "The customer is very important to me. Search how to help him create his own project.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "The customer is very important to me. Search how to help him create his own project.",
+    },
+    {
+        title: "React Design",
+        description: "The customer is very important to me. Search how to help him create his own project.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "amrhe4030@gmail.com",
+    YOUR_FONE: "(+20)01009152403",
+    description: "I am a talented, ambitious and hardworking person, with extensive web development skills and experience.Furthermore, I am adept at handling multiple tasks on a. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Amr-helmy0",
+    facebook: "https://www.facebook.com/mero.helmy.1612/",
+    linkedin: "https://www.facebook.com/mero.helmy.1612/",
+    twitter: "https://twitter.com/amrhelm0",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
